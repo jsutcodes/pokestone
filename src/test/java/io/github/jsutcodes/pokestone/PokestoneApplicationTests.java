db170a4077e9e27cee80d443dd784dc87b0a8c0d
Jordan Sutton <jorsutton92@gmail.com>
2021-10-23 06:06:34 -0600

    init

---
+++ src/test/java/io/github/jsutcodes/pokestone/PokestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jsutcodes.pokestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
